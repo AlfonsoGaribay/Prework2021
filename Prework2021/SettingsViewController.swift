ecd6c8ae6b09d70e9c4c76af61d90da434b9c511
Alfonso <alfonso.garibay@sjsu.edu>
2021-02-07 10:05:59 -0800

    Added in a settings view controller and a button to get to it from the tip calculator. needs settings and buttons but for now it is there.

---
+++ Prework2021/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  Prework2021
+//
+//  Created by Alfonso Garibay on 2/7/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
